c531095f3f8c6ccf754ae206ff0a115a80c88f4d
cu4tro-dgb <saekaronw@gmail.com>
2024-09-20 17:28:09 -0500

    add extend style tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [],
+  theme: {
+    extend: {
+      borderRadius: {
+        '4xl': '2rem'
+      }
+    }
+  },
+  plugins: []
+} satisfies Config
